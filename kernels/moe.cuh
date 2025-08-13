3a5b97ef78ca23cd5788b21eb0cac005159ee7ff
Muyu He <muyuhe0327@gmail.com>
2025-08-12 19:55:26 -0700

    Implemented topk selection from router logits

--- kernels/moe.cuh
+++ kernels/moe.cuh
@@ -46,13 +46,15 @@ __global__ void moe_router_softmax_topk_kernel(
     int token_idx = blockIdx.x;  // each block processes one token 
     if (token_idx >= num_tokens) return;
 
-    extern __shared__ float shared_mem[]; // stores scores and indices in shared memory
+    // for each block/token, allocate num_experts space for ind/scores in shared memory
+    extern __shared__ float shared_mem[]; 
     float* scores = shared_mem;
-    int* indices = (int*)&scores[num_experts];
+    int* indices = (int*)&scores[num_experts]; 
 
     float max_val = -FLT_MAX;
     // traverse across all experts for one token
     // each thread processes one expert
+    // each iteration processes one "expert chunk", chunk size is blockDim.x
     for (int i = threadIdx.x; i < num_experts; i += blockDim.x) { 
         float val = (float)router_logits[token_idx * num_experts + i]; // get logit for this expert
         scores[i] = val;
@@ -64,16 +66,70 @@ __global__ void moe_router_softmax_topk_kernel(
     max_val = BlockReduce(temp_storage).Reduce(max_val, cub::Max()); // reduce max value on all experts for this token
 
     if (threadIdx.x == 0) {
-        // TODO: will this clash with indices?
-        shared_mem[num_experts] = max_val;  // store max value for this token
+        // store max value for softmax
+        shared_mem[num_experts] = max_val; // will be overwritten by indices[0]
     }
     __syncthreads();
     max_val = shared_mem[num_experts];
 
+    // compute softmax with max_val offset
     float sum = 0.0f;
     for (int i = threadIdx.x; i < num_experts; i += blockDim.x) {
-        
+        float exp_val = expf(scores[i] - max_val);
+        scores[i] = exp_val;
+        sum += exp_val;
     }
+
+    __syncthreads();
+    sum = BlockReduce(temp_storage).Sum(sum);
+    if (threadIdx.x == 0) {
+        shared_mem[num_experts + 1] = sum; // will be overwritten by indices[1]
+    }
+
+    __syncthreads();
+    sum = shared_mem[num_experts + 1];
+    for (int i = threadIdx.x; i < num_experts; i += blockDim.x) {
+        scores[i] = scores[i] / sum;
+        indices[i] = i;
+    }
+
+    __syncthreads();
+
+    // parallel topk
+    // TODO: change to cub:BlockRadixSort for efficiency
+    if (threadIdx.x == 0) {
+        // bubble sort
+        for (int i = 0; i < num_experts_per_tok; i++) {
+            int max_idx = i;
+            float max_score = scores[i];
+            for (int j = i + 1; j < num_experts; j++) {
+                if (scores[j] > max_score) {
+                    max_score = scores[j];
+                    max_idx = j;
+                }
+            }
+            if (max_idx != i) {
+                float temp_score = scores[i];
+                int temp_idx = indices[i];
+                scores[i] = scores[max_idx];
+                indices[i] = indices[max_idx];
+                scores[max_idx] = temp_score;
+                indices[max_idx] = temp_idx;
+            }
+        } // first k indices in indices are now topk
+    }
+
+    // Normalize topk and write to selected_experts
+    float tpok_sum = 0.0f;
+    for (int i = 0; i < num_experts_per_tok; i++) {
+        topk_sum += scores[i];
+    }
+
+    for (int i = 0; i < num_experts_per_tok; i++) {
+        selected_experts[token_idx * num_experts_per_tok + i] = indices[i];
+        routing_weights[token_idx * num_experts_per_tok + i] = (floatX)(scores[i] / topk_sum); 
+    }
+
 }
 
 __global__ void moe_router_kernel(
